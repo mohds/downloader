03725b51bab641c1d164bda805286e32b6084ef2
Mohammad Salloum <m.salloum@ALMAYADEEN.NET>
2017-06-23 06:47:21 +0300

    initial arch

--- downloader/Form1.Designer.cs
+++ downloader/Form1.Designer.cs
@@ -29,44 +29,50 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(92, 95);
+            this.button1.Location = new System.Drawing.Point(12, 13);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(87, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(53, 58);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(161, 20);
-            this.textBox1.TabIndex = 1;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(112, 136);
+            this.label1.Location = new System.Drawing.Point(67, 71);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Status";
+            this.label1.Size = new System.Drawing.Size(32, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(214, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Status";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(331, 322);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Downloader";
             this.ResumeLayout(false);
@@ -77,8 +83,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- downloader/Form1.cs
+++ downloader/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.Mail;
@@ -20,36 +21,76 @@ namespace downloader
     {
         String message_text_file = "message_text.txt";
         String links_text_file = "links.txt";
+        String downloads_folder = "C:/downloads";
+        String destination_folder = @"\\ccoder2\MediaCoder\Sharer IT";
+        List<OpenPop.Mime.Message> messages_currently_processing = new List<OpenPop.Mime.Message>();
+        BackgroundWorker backgroundWorker1 = new BackgroundWorker();
+        BackgroundWorker backgroundWorker2 = new BackgroundWorker();
 
         public Form1()
         {
             InitializeComponent();
-            get_last_message();
+            
+            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
+            backgroundWorker1.ProgressChanged += backgroundWorker1_ProgressChanged;
+            backgroundWorker1.WorkerReportsProgress = true;
+
         }
-        void get_last_message()
+        void process_last_message()
         {
             var client = new Pop3Client();
             client.Connect("pop.gmail.com", 995, true);
             client.Authenticate("send2sharer@gmail.com", "manipulate");
             var count = client.GetMessageCount();
-            OpenPop.Mime.Message message = client.GetMessage(count);
-            Console.WriteLine(message.Headers.Subject);
 
-            FindPlainTextInMessage(message);
-
-            message_parser(message_text_file);
-
-            StreamReader reader = File.OpenText(links_text_file);
-            String link = "";
-            while ((link = reader.ReadLine()) != null)
+            if (count > 0)
             {
-                if (link.Contains("youtube") || link.Contains("youtu.be"))
+                OpenPop.Mime.Message message = client.GetMessage(count);
+
+                if (!messages_currently_processing.Contains(message))
                 {
-                    download_youtube_video(link);
+                    // add message to currently_processing list
+                    messages_currently_processing.Add(message);
+
+                    Console.WriteLine(message.Headers.Subject);
+
+                    FindPlainTextInMessage(message);
+
+                    message_parser(message_text_file);
+
+                    StreamReader reader = File.OpenText(links_text_file);
+                    String link = "";
+                    while ((link = reader.ReadLine()) != null)
+                    {
+                        if (link.Contains("youtube") || link.Contains("youtu.be"))
+                        {
+                            BackgroundWorker worker = new BackgroundWorker();
+                            worker.DoWork += new DoWorkEventHandler(backgroundWorker2_DoWork);
+                            worker.ProgressChanged += new ProgressChangedEventHandler(backgroundWorker2_ProgressChanged);
+                            worker.WorkerReportsProgress = true;
+                            worker.RunWorkerAsync(link);
+                        }
+                        Thread.Sleep(1000);
+                    }
+
+                    // delete message to not process again
+                    try
+                    {
+                        client.DeleteMessage(count);
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                    client.Disconnect();
+
+                    // remove from currently processing messages
+                    messages_currently_processing.Remove(message);
                 }
             }
-
+            Thread.Sleep(1000);
         }
+
         void FindPlainTextInMessage(OpenPop.Mime.Message message)
         {
             MessagePart plainText = message.FindFirstPlainTextVersion();
@@ -86,8 +127,12 @@ namespace downloader
                 }
             }
         }
-        void download_youtube_video(String youtube_link)
+        String download_youtube_video(String youtube_link)
         {
+            
+            String download_path = "";
+            String file_name = "";
+            String video_title = "";
             /*
              * Get the available video formats.
              * We'll work with them in the video and audio download examples.
@@ -111,16 +156,98 @@ namespace downloader
              * The first argument is the video to download.
              * The second argument is the path to save the video file.
              */
-            var videoDownloader = new VideoDownloader(video, Path.Combine("M:/Downloads", video.Title + video.VideoExtension));
+            file_name = video.Title + video.VideoExtension;
+            
+            video_title = video.Title;
+            video_title = video_title.Substring(0, video_title.Length / 2);
+            
+            video_title = video_title.Replace(":", "");
+            video_title = video_title.Replace(@"\", "");
+            video_title = video_title.Replace("/", "");
+            video_title = video_title.Replace('"', ' ').Trim();
+
+            download_path = Path.Combine(downloads_folder, video_title + video.VideoExtension);
+            file_name = video_title + video.VideoExtension;
+            var videoDownloader = new VideoDownloader(video, download_path);
 
             // Register the ProgressChanged event and print the current progress
-            //videoDownloader.DownloadProgressChanged += (sender, args) => Console.WriteLine(args.ProgressPercentage);
-
+            videoDownloader.DownloadProgressChanged += (sender, args) => Console.WriteLine(args.ProgressPercentage);
+            
             /*
              * Execute the video downloader.
              * For GUI applications note, that this method runs synchronously.
              */
-            videoDownloader.Execute();
+            try
+            {
+                videoDownloader.Execute();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+
+                //try again
+                try
+                {
+                    videoDownloader.Execute();
+                }
+                catch
+                {
+
+                }
+            }
+            // move file to sharer
+            String source_path = downloads_folder + "/" + file_name;
+            String destination_path = destination_folder + "\\" + file_name;
+            try
+            {
+                File.Move(source_path, destination_path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Could not move file. Error:\n" + ex.ToString());
+            }
+
+            return file_name;
+        }        
+        private int get_number_of_chars_in_string(String line, char char_to_count)
+        {
+            int count = 0;
+            for(int i = 0; i < line.Count(); i++)
+            {
+                if(line[i] == char_to_count)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        private void start_processing()
+        {
+            while (true)
+            {
+                process_last_message();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            start_processing();
+        }
+        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            // label1.Text = e.ProgressPercentage.ToString();
+        }
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            String link = e.Argument.ToString();
+            download_youtube_video(link);
+        }
+        private void backgroundWorker2_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            // label1.Text = e.ToString();
         }
     }
 }
