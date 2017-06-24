e97e807e31a29869d0dfc1aaa4af7b4c631c5ea1
Mohammad Salloum <m.salloum@ALMAYADEEN.NET>
2017-06-24 06:44:19 +0300

    youtube stability + GUI feedback

--- downloader/Form1.Designer.cs
+++ downloader/Form1.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -44,30 +44,33 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(67, 71);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Title";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(214, 71);
+            this.label2.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(280, 70);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.Size = new System.Drawing.Size(53, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Status";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(70, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Title";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
-            this.ClientSize = new System.Drawing.Size(331, 322);
+            this.ClientSize = new System.Drawing.Size(582, 455);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -83,8 +86,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- downloader/Form1.cs
+++ downloader/Form1.cs
@@ -14,6 +14,7 @@ using OpenPop.Mime;
 using System.IO;
 using System.Text.RegularExpressions;
 using YoutubeExtractor;
+using System.Net;
 
 namespace downloader
 {
@@ -24,17 +25,51 @@ namespace downloader
         String downloads_folder = "C:/downloads";
         String destination_folder = @"\\ccoder2\MediaCoder\Sharer IT";
         List<OpenPop.Mime.Message> messages_currently_processing = new List<OpenPop.Mime.Message>();
+        List<String> downloaded_videos = new List<string>();
+        List<String> failed_links = new List<string>();
+        String sender = "";
         BackgroundWorker backgroundWorker1 = new BackgroundWorker();
-        BackgroundWorker backgroundWorker2 = new BackgroundWorker();
+        BackgroundWorker backgroundWorker2 = new BackgroundWorker(); // leave it be
+        BackgroundWorker backgroundWorker3 = new BackgroundWorker();
+        List<String> workers_busy = new List<string>(); // a list in which each worker states that they are busy
+        List<Label> labels = new List<Label>(); // to keep track of label positions
+        List<Label> label_statuses = new List<Label>(); // status manipulation
+        List<String> labels_to_add = new List<string>();
+        int status_updated = 0; // changes to the index which needs an update
+
+        List<String> allowed_senders = new List<string>();
 
         public Form1()
         {
             InitializeComponent();
-            
+
+            populate_allowed_senders();
+
             backgroundWorker1.DoWork += backgroundWorker1_DoWork;
             backgroundWorker1.ProgressChanged += backgroundWorker1_ProgressChanged;
             backgroundWorker1.WorkerReportsProgress = true;
 
+            backgroundWorker3.DoWork += backgroundWorker3_DoWork;
+            backgroundWorker3.ProgressChanged += backgroundWorker3_ProgressChanged;
+            backgroundWorker3.WorkerReportsProgress = true;
+
+
+
+            labels.Add(label1);
+        }
+        private void populate_allowed_senders()
+        {
+            allowed_senders.Add("m.salloum@almayadeen.net");
+            allowed_senders.Add("z.hteit@almayadeen.net");
+            allowed_senders.Add("s.souwaidan@almayadeen.net");
+            allowed_senders.Add("w.elahmar@almayadeen.net");
+            allowed_senders.Add("m.awad@almayadeen.net");
+            allowed_senders.Add("ma.saad@almayadeen.net");
+            allowed_senders.Add("j.krayem@almayadeen.net");
+            allowed_senders.Add("h.chreim@almayadeen.net");
+            allowed_senders.Add("a.hammoud@almayadeen.net");
+            allowed_senders.Add("a.kobeissi@almayadeen.net");
+            allowed_senders.Add("r.dagher@almayadeen.net");
         }
         void process_last_message()
         {
@@ -45,32 +80,48 @@ namespace downloader
 
             if (count > 0)
             {
+                downloaded_videos.Clear();
+                failed_links.Clear();
+                workers_busy.Clear();
+
                 OpenPop.Mime.Message message = client.GetMessage(count);
 
                 if (!messages_currently_processing.Contains(message))
                 {
+                    // grab sender
+                    sender = message.Headers.From.Address;
+
+                    // security
+                    if (!(allowed_senders.Contains(sender)))
+                    {
+                        return;
+                    }
+
                     // add message to currently_processing list
                     messages_currently_processing.Add(message);
 
-                    Console.WriteLine(message.Headers.Subject);
+                    Console.WriteLine(message.Headers.Subject + " From: " + sender);
 
                     FindPlainTextInMessage(message);
 
-                    message_parser(message_text_file);
+                    if (message_parser(message_text_file)) // returns true on success
+                    { 
 
-                    StreamReader reader = File.OpenText(links_text_file);
-                    String link = "";
-                    while ((link = reader.ReadLine()) != null)
-                    {
-                        if (link.Contains("youtube") || link.Contains("youtu.be"))
+                        StreamReader reader = File.OpenText(links_text_file);
+                        String link = "";
+                        while ((link = reader.ReadLine()) != null)
                         {
-                            BackgroundWorker worker = new BackgroundWorker();
-                            worker.DoWork += new DoWorkEventHandler(backgroundWorker2_DoWork);
-                            worker.ProgressChanged += new ProgressChangedEventHandler(backgroundWorker2_ProgressChanged);
-                            worker.WorkerReportsProgress = true;
-                            worker.RunWorkerAsync(link);
+                            if (link.Contains("youtube") || link.Contains("youtu.be"))
+                            {
+                                BackgroundWorker worker = new BackgroundWorker();
+                                worker.DoWork += new DoWorkEventHandler(backgroundWorker2_DoWork);
+                                worker.ProgressChanged += new ProgressChangedEventHandler(backgroundWorker2_ProgressChanged);
+                                worker.WorkerReportsProgress = true;
+                                worker.RunWorkerAsync(link);
+                                workers_busy.Add("busy");
+                            }
+                            Thread.Sleep(1000);
                         }
-                        Thread.Sleep(1000);
                     }
 
                     // delete message to not process again
@@ -87,6 +138,12 @@ namespace downloader
                     // remove from currently processing messages
                     messages_currently_processing.Remove(message);
                 }
+                while(workers_busy.Count > 0)
+                {
+                    // halt while workers work
+                }
+                // all workers done
+                send_email(sender);
             }
             Thread.Sleep(1000);
         }
@@ -100,8 +157,9 @@ namespace downloader
                 plainText.Save(new FileInfo(message_text_file));
             }
         }
-        void message_parser(String file_name)
+        bool message_parser(String file_name)
         {
+            bool return_me = false;
             try
             {
                 File.Delete(links_text_file);
@@ -123,9 +181,11 @@ namespace downloader
                     using (StreamWriter writer = File.AppendText(links_text_file))
                     {
                         writer.WriteLine(link);
+                        return_me = true;
                     }
                 }
             }
+            return return_me;
         }
         String download_youtube_video(String youtube_link)
         {
@@ -137,11 +197,28 @@ namespace downloader
              * Get the available video formats.
              * We'll work with them in the video and audio download examples.
              */
-            IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(youtube_link);
+            IEnumerable<VideoInfo> videoInfos;
+            try
+            {
+                videoInfos = DownloadUrlResolver.GetDownloadUrls(youtube_link);
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("GetDownloadUrls() failed, trying again...");
+                videoInfos = DownloadUrlResolver.GetDownloadUrls(youtube_link);
+                
+            }
+            
             /*
              * Select the first .mp4 video with 360p resolution
              */
             VideoInfo video = videoInfos.First(info => info.VideoType == VideoType.Mp4 && info.Resolution == 360);
+
+            double video_size = extract_video_size(video.DownloadUrl);
+            // convert to MB
+            video_size = video_size / 1000000;
 
             /*
              * If the video has a decrypted signature, decipher it
@@ -171,30 +248,61 @@ namespace downloader
             var videoDownloader = new VideoDownloader(video, download_path);
 
             // Register the ProgressChanged event and print the current progress
-            videoDownloader.DownloadProgressChanged += (sender, args) => Console.WriteLine(args.ProgressPercentage);
-            
+            // videoDownloader.DownloadProgressChanged += (sender, args) => Console.WriteLine(args.ProgressPercentage);
+
             /*
              * Execute the video downloader.
              * For GUI applications note, that this method runs synchronously.
              */
+
+            // index of status for later use
+            int index_status = 0;
+
             try
             {
-                videoDownloader.Execute();
+                Console.WriteLine("Started downloading: " + video_title);
+                add_row(video_title, "Started");
+                index_status = label_statuses.Count - 1;
+
+                videoDownloader.DownloadProgressChanged += (sender, args) => {
+                    //label_statuses[index_status].SafeInvoke(d => d.Text = args.ProgressPercentage.ToString());
+                    String percentage_downloaded_str = args.ProgressPercentage.ToString();
+                    double percentage_downloaded = Convert.ToDouble(percentage_downloaded_str);
+                    
+                    percentage_downloaded = percentage_downloaded / 100;
+                    double size_downloaded = percentage_downloaded * video_size;
+                    video_size = Math.Round(video_size, 2);
+                    size_downloaded = Math.Round(size_downloaded, 2);
+                    String display_me = size_downloaded.ToString() + "MB / " + video_size.ToString() + "MB";
+                    label_statuses[index_status].SafeInvoke(d => d.Text = display_me);
+                };
+
+                videoDownloader.Execute();                
             }
             catch(Exception ex)
             {
                 Console.WriteLine(ex.ToString());
 
-                //try again
+                // try again
                 try
                 {
+                    Console.WriteLine("Started downloading: " + video_title);
+                    add_row(video_title, "Started");
+                    index_status = label_statuses.Count - 1;
                     videoDownloader.Execute();
                 }
                 catch
                 {
-
+                    // exit function
+                    return file_name;
                 }
             }
+
+            // to check title-status harmony
+            //label_statuses[index_status].SafeInvoke(d => d.Text = video_title);
+            //label_statuses[index_status].Text = index_status.ToString();
+            //this.SafeInvoke(d => d.Refresh());
+
             // move file to sharer
             String source_path = downloads_folder + "/" + file_name;
             String destination_path = destination_folder + "\\" + file_name;
@@ -207,8 +315,33 @@ namespace downloader
                 Console.WriteLine("Could not move file. Error:\n" + ex.ToString());
             }
 
+            // add name to downloaded list
+            downloaded_videos.Add(video_title);
+
+            // update interface
+            // label_statuses[index_status].Text = "Done";
+            label_statuses[index_status].SafeInvoke(d => d.Text = "Done");
+            this.SafeInvoke(d => d.Refresh());
+            // status_updated = index_status; // it is only required to change it from 0 (so far)
+
             return file_name;
-        }        
+        }
+        private int extract_video_size(String video_url)
+        {
+            int video_size = 1;
+            String[] sections;
+            sections = video_url.Split('&');
+            foreach(var section in sections)
+            {
+                if (section.Contains("clen="))
+                {
+                    String video_size_str = section.Substring(section.IndexOf('=') + 1, (section.Length - 1) - section.IndexOf('='));
+                    video_size = Int32.Parse(video_size_str);
+                    break;
+                }
+            }
+            return video_size;
+        }
         private int get_number_of_chars_in_string(String line, char char_to_count)
         {
             int count = 0;
@@ -231,6 +364,33 @@ namespace downloader
         private void button1_Click(object sender, EventArgs e)
         {
             backgroundWorker1.RunWorkerAsync();
+            button1.Enabled = false;
+            backgroundWorker3.RunWorkerAsync();
+            
+        }
+        private void add_row(String title, String status)
+        {
+            Label label_title = new Label();
+            Label label_status = new Label();
+            label_title.Width = 100;
+            label_status.Width = 300;
+            int workers_count = workers_busy.Count + 1;
+            label_title.Text = title;
+            label_status.Text = status;
+            int y_of_last_label = labels[labels.Count - 1].Location.Y;
+            label_title.Left = label1.Location.X;
+            label_title.Top = y_of_last_label + 30;
+            label_status.Left = label2.Location.X;
+            label_status.Top = y_of_last_label + 30;
+            //this.Controls.Add(label_status);
+            //this.Controls.Add(label_title);
+            this.SafeInvoke(d => d.Controls.Add(label_status));
+            this.SafeInvoke(d => d.Controls.Add(label_title));
+            this.SafeInvoke(d => d.Refresh());
+            labels.Add(label_title);
+
+            // keeping labels within reach
+            label_statuses.Add(label_status);
         }
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
@@ -243,11 +403,76 @@ namespace downloader
         private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
         {
             String link = e.Argument.ToString();
-            download_youtube_video(link);
+            //try
+            //{
+                download_youtube_video(link);
+            //}
+            /*catch (Exception ex)
+            {
+                Console.WriteLine("Could not download: " + link);
+                failed_links.Add(link);
+            }*/
+            if (workers_busy.Count > 0)
+            {
+                workers_busy.Remove(workers_busy[0]);
+            }
         }
         private void backgroundWorker2_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
         {
-            // label1.Text = e.ToString();
+
         }
+        private void backgroundWorker3_DoWork(object sender, DoWorkEventArgs e)
+        {
+            /*
+            while (true)
+            {
+                // interface handling
+                if (labels_to_add.Count > 0)
+                {
+                    int index = labels_to_add.Count - 1;
+                    add_row(labels_to_add[index], label_statuses[index].Text);
+                    labels_to_add.Remove(labels_to_add[index]);
+                }
+                if (status_updated != 0)
+                {
+                    this.SafeInvoke(d => d.Refresh());
+                    status_updated = 0;
+                }
+                Thread.Sleep(100);
+            }*/
+        }
+        private void backgroundWorker3_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+
+
+        }
+        private void send_email(String to)
+        {
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential("send2sharer@gmail.com", "manipulate"),
+                EnableSsl = true
+            };
+            String message_body = "";
+            message_body += "On sharer:\n";
+            foreach(var title in downloaded_videos)
+            {
+                message_body += title + "\n";
+            }
+            if(failed_links.Count > 0)
+            {
+                message_body += "\nFailed links:\n";
+                foreach(var link in failed_links)
+                {
+                    message_body += link + "\n";
+                }
+            }
+            message_body += "\nRegards,\n";
+            message_body += "Download Assistant\n";
+            client.Send("send2sharer@gmail.com", to, "On sharer", message_body);
+            Console.WriteLine("Email sent");
+        }
+
+
     }
 }
